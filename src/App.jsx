716dea7c9515be0f9664d0c65f12de3ac6e67ff0
Ayanrinde Olutayo Stephen <olutayostephen@gmail.com>
2024-06-04 15:30:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='home' element={<Home/>}/> 
+        <Route path='/*' element={<Home/>}/> 
+      </Routes>
+    </>
+  )
+}
+
+export default App
